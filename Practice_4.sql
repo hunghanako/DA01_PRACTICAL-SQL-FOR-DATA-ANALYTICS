5f3969083a4085437ea33c4f331808b48a534d66
hunghanako <hunghanako@gmail.com>
2024-11-12 14:50:42 +0700

    Update Practice_4.sql

--- Practice_4.sql
+++ Practice_4.sql
@@ -16,9 +16,17 @@ Case
     Else 'Yes'
 End Triangle
 From Triangle
---BÀI 3 (đợi chữa)
+--BÀI 3 (đề bài sai nên bỏ qua)
 SELECT 
 Round(Cast(
 (Sum(Case When call_category is Null or call_category ='n/a' then 1 Else 0 END)
 /sum(CASE When call_category is not Null and call_category <>'n/a' then 1 Else 0 End))*100 as decimal),1) 
 as uncategoried_call_pct
+--BÀI 4 làm rồi
+--BÀi 5
+Select survived,
+Sum(Case When pclass ='1' then 1 else 0 End) first_class,
+Sum(Case when pclass ='2' then 1 else 0 end) as second_class,
+Sum(case when pclass ='3' then 1 else 0 end) as third_class
+From titanic
+Group by survived
