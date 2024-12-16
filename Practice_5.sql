4a22e2f7dad077e612621c5fbe1be72e13b2e410
hunghanako <hunghanako@gmail.com>
2024-12-17 00:34:57 +0700

    Update Practice_5.sql

--- Practice_5.sql
+++ Practice_5.sql
@@ -4,3 +4,23 @@ from city
 JOIN country on CITY.Countrycode=COUNTRY.CODE
 Group by COUNTRY.Continent
 --BÀI 2
+SELECT 
+  ROUND(CAST(COUNT(texts.email_id) AS DECIMAL)
+  /COUNT(DISTINCT emails.email_id),2) AS activation_rate
+FROM emails
+LEFT JOIN texts
+  ON emails.email_id = texts.email_id
+  AND texts.signup_action = 'Confirmed'
+-- Bài 3 (xem lại filter)
+SELECT age_breakdown.age_bucket,
+ROUND(SUM(activities.time_spent) FILTER (WHERE activities.activity_type = 'send') / SUM(activities.time_spent) *100, 2)
+AS send_perc, --- = time spent sending / (Time spent sending + Time spent opening)
+ROUND (SUM(activities.time_spent) FILTER (WHERE activities.activity_type = 'open') / SUM(activities.time_spent)
+ *100 , 2) AS open_perc  --- = time spent opening / (Time spent sending + Time spent opening)
+FROM activities
+INNER JOIN age_breakdown
+ON activities.user_id = age_breakdown.user_id --- chung 2 bang
+WHERE activities.activity_type IN ('open', 'send') --- dieu kien loc
+GROUP BY age_breakdown.age_bucket --- cot chinh
+
+--Bài 4
